636ffbc04563157b57e05dece24e65e33a6dd536
BlueVelocity <josephrodney@hotmail.com>
2023-08-01 13:44:59 -0400

    Refine logic and fix bugs
    Changed logic so that a tie is no longer counted as a point
    Now displays winner at first to 3, or until 5 games are complete

--- script.js
+++ script.js
@@ -29,36 +29,68 @@ function promptUser() {
 
 //function to run the game
 function runGame() {
+    //counter of wins for player
+    playerWinCounter = 0;
+    //counter of wins for computer
+    computerWinCounter = 0;
+
     //for loop to run the game 5 times, prompt user each loop
     for (i = 0; i < 5; i++) {
 
+        //prompt the user each round
         let playerSelection = promptUser();
 
         //get computerChoice, store in variable
         let computerSelection = getComputerChoice();
+
+        //function to keep score and display winner/loser
+        function winCounter(winner) {
+
+            if (winner  === 'p') {
+                console.log(`Computer selected ${computerSelection}. You win this round!`)
+                playerWinCounter++
+            } else if (winner === 'c'){
+                console.log(`Computer selected ${computerSelection}. You lose this round...`);
+                computerWinCounter++
+            } 
+        }
 
         //logic for game win or lose
         if (computerSelection === playerSelection) {
             console.log('It\'s a tie!');
         } else if (computerSelection === 'rock') {
             if (playerSelection === 'paper') {
-                console.log(`Computer selected ${computerSelection}. You Win!`);
+                winCounter('p');
             } else {
-                console.log(`Computer selected ${computerSelection}. You Lose!`);
+                winCounter('c')
             }
         } else if (computerSelection === 'paper') {
             if (playerSelection === 'scissors') {
-                console.log(`Computer selected ${computerSelection}. You Win!`);
+                winCounter('p');
             } else {
-                console.log(`Computer selected ${computerSelection}. You Lose!`);
+                winCounter('c');
             }
         } else if (computerSelection === 'scissors') {
             if (playerSelection === 'rock') {
-                console.log(`Computer selected ${computerSelection}. You Win!`);
+                winCounter('p');
             } else {
-                console.log(`Computer selected ${computerSelection}. You Lose!`);
+                winCounter('c')
             }
         }
+
+        //breaks the loop if a winner is decided, best 3 of 5
+        if (playerWinCounter === 3 || computerWinCounter === 3) {
+            break;
+        }
+    }
+
+    //check for winner, display winner with score
+    if (playerWinCounter > computerWinCounter) {
+        console.log(`You win! You: ${playerWinCounter}, Computer: ${computerWinCounter}`);
+    } else if (playerWinCounter < computerWinCounter) {
+        console.log(`You Lost... Computer: ${computerWinCounter}, You: ${playerWinCounter}`);
+    } else {
+        console.log(`You tied! You: ${playerWinCounter}, Computer: ${computerWinCounter}`);
     }
 }
 
