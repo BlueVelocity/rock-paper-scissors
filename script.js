e7d722edf1cea37e19e922764745922740129324
BlueVelocity <josephrodney@hotmail.com>
2023-08-20 13:24:24 -0400

    Refactor javascript code
    Refactored javascript code for my future understanding when
    I'll ultimately have to make this look more professional

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+
+let computerOutputDiv = document.getElementById("computer-output-div");
+let playerInputDiv = document.getElementById("input-div");
+let computerScoreCounter = document.getElementById("computer-score");
+let playerScoreCounter = document.getElementById("player-score");
+let computerOutputFigure = document.getElementById('computer-output-figure')
+let victoryAnnouncementOutput = document.getElementById('victory-announcement');
+let buttons = document.querySelectorAll(".input-button");
+
+let playerScore= 0;
+let computerScore = 0;
+
+//audio handlers
 let pageAudio = document.getElementById('pageAudio');
+let attackAudio = document.getElementById('attackAudio');
 pageAudio.volume = 0.1;
 firstHover = true;
 window.addEventListener('click', () => {
@@ -7,11 +21,6 @@ window.addEventListener('click', () => {
         setTimeout( () => pageAudio.play(), 260000);
     }
 })
-
-let attackAudio = document.getElementById('attackAudio');
-attackAudio.volume = 1;
-
-let buttons = document.querySelectorAll(".input-button");
 
 buttons.forEach( (button) => {button.addEventListener('click', ( () => playerButtonClick(button)))})
 
@@ -25,94 +34,58 @@ function playerButtonClick(button) {
     }, 75)
 }
 
-let computerOutputDiv = document.getElementById("computer-output-div");
-let playerInputDiv = document.getElementById("input-div");
-
-let computerScoreCounter = document.getElementById("computer-score");
-let playerScoreCounter = document.getElementById("player-score");
-
-let computerOutputFigure = document.getElementById('computer-output-figure')
-
-let victoryAnnouncementOutput = document.getElementById('victory-announcement');
-
-//counter of wins for player
-let playerScore= 0;
-//counter of wins for computer
-let computerScore = 0;
-
-//randomly generate computer answer, return answer
 function getComputerChoice() {
     let x = Math.random()
     if (x < 0.333) {
         computerOutputFigure.setAttribute('src', './photo/fire_rune.png');
         return 'rock';
     } else if (x < 0.666) {
-        computerOutputFigure.setAttribute('src', './photo/water_rune.png')
+        computerOutputFigure.setAttribute('src', './photo/water_rune.png');
         return 'paper';
     } else if (0.666 < x) {
-        computerOutputFigure.setAttribute('src', './photo/nature_rune.png')
+        computerOutputFigure.setAttribute('src', './photo/nature_rune.png');
         return 'scissors';
     }
 }
 
-//function to run the game
 function runGame(playerSelection) {
-
-    //get computerChoice, store in variable
+    
     let computerSelection = getComputerChoice();
-
-    //function to keep score and display winner/loser
+    //keeps score and displays winner/loser
     function announcer(winner) {
-        if (winner  === 'p') {
-            console.log(`Computer selected ${computerSelection}. You win this round!`)
+        if (winner === 'tie') {
+            changeIndicatorBackground('tie');
+        } else if (winner  === 'p') {
             changeIndicatorBackground('playerWin');
             playerScore++
             playerScoreCounter.textContent = `${playerScore}`;
         } else if (winner === 'c'){
-            console.log(`Computer selected ${computerSelection}. You lose this round...`);
             changeIndicatorBackground('computerWin');
-            computerScore++
+            computerScore++;
             computerScoreCounter.textContent = `${computerScore}`;
         } 
     }
-
-    let optionsArray = ['rock', 'paper', 'scissors']
     //logic for game win or lose
     if (computerSelection === playerSelection) {
-        console.log('It\'s a tie!');
-        changeIndicatorBackground('tie');
-    } else if (computerSelection === 'rock') {
-        if (playerSelection === 'paper') {
-            announcer('p');
-        } else {
-            announcer('c')
-        }
-    } else if (computerSelection === 'paper') {
-        if (playerSelection === 'scissors') {
-            announcer('p');
-        } else {
-            announcer('c');
-        }
-    } else if (computerSelection === 'scissors') {
-        if (playerSelection === 'rock') {
-            announcer('p');
-        } else {
-            announcer('c')
-        }
+        announcer('tie');
+    } else if (
+        (computerSelection === 'rock' && playerSelection === 'paper') || 
+        (computerSelection === 'paper' && playerSelection === 'scissors') || 
+        (computerSelection === 'scissors' && playerSelection === 'rock')) {
+        announcer('p');
+    } else {
+        announcer('c');
     }
     
     if (playerScore === 5 || computerScore === 5) {
-        winOrLose()
+        winOrLose();
     }
-
 }
 
 function winOrLose() {
     if (playerScore > computerScore) {
-        console.log(`You win! You: ${playerScore}, Computer: ${computerScore}`);
         changeIndicatorBackground('playerVictory');
     } else {
-        console.log(`You Lost... Computer: ${computerScore}, You: ${playerScore}`);
         changeIndicatorBackground('computerVictory');
     } 
     resetGame();
@@ -120,37 +93,39 @@ function winOrLose() {
 
 function changeIndicatorBackground(winner) {
     if (winner === 'playerWin') {
-        setIndicatorDivColors('lightgreen', 'pink')
-        attackAudio.currentTime = 0;
-        attackAudio.play();
+        setIndicatorDivColors('lightgreen', 'pink');
+        resetAudioTimer(attackAudio);
     } else if (winner === 'computerWin') {
-        setIndicatorDivColors('pink', 'lightgreen')
-        attackAudio.currentTime = 0;
-        attackAudio.play();
+        setIndicatorDivColors('pink', 'lightgreen');
+        resetAudioTimer(attackAudio);
     } else if (winner === 'tie') {
-        setIndicatorDivColors('lightblue', 'lightblue')
+        setIndicatorDivColors('lightblue', 'lightblue');
     } else if (winner === 'playerVictory') {
-        setIndicatorDivColors('yellow')
+        setIndicatorDivColors('yellow');
         victoryAnnouncementOutput.textContent = 'You are the winner!';
         setTimeout( () => victoryAnnouncementOutput.textContent = 'Who will win?', 3000);
     } else if (winner === 'computerVictory') {
-        setIndicatorDivColors(undefined,'yellow')
+        setIndicatorDivColors(undefined,'yellow');
         victoryAnnouncementOutput.textContent = 'The AI has won...';
         setTimeout( () => victoryAnnouncementOutput.textContent = 'Who will win?', 3000);
     } else {
-        console.log('error with changeIndicatorBackground()')
+        console.log('error with changeIndicatorBackground()');
     }
 }
 
 function setIndicatorDivColors(playerColor, computerColor) {
-    playerInputDiv.setAttribute('style', `background-color: ${playerColor}`)
-    computerOutputDiv.setAttribute('style', `background-color: ${computerColor}`)
+    playerInputDiv.setAttribute('style', `background-color: ${playerColor}`);
+    computerOutputDiv.setAttribute('style', `background-color: ${computerColor}`);
+}
+
+function resetAudioTimer(audio) {
+    audio.currentTime = 0;
+    audio.play();
 }
 
 function resetGame() {
     playerScoreCounter.textContent = '0';
     computerScoreCounter.textContent = '0';
-    victoryAnnouncementOutput.textContent 
     playerScore = 0;
     computerScore = 0;
     console.log('game reset');
