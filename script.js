cf58f6264a8e9e09056d7ebaff955b92ef209f14
BlueVelocity <josephrodney@hotmail.com>
2023-08-20 00:12:13 -0400

    Play music using javascript
    Switched the autoplay and loop functions of the bakground music to javascript

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let pageAudio = document.getElementById('pageAudio');
 pageAudio.volume = 0.1;
+pageAudio.play();
+setTimeout( () => pageAudio.play(), 260000);
 let attackAudio = document.getElementById('attackAudio');
 attackAudio.volume = 1;
 
